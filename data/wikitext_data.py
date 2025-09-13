a0221a29e78394ea3a3a166d99e612bb898d4a47
akumaluffy <yliu0602@gmail.com>
2025-09-13 15:00:15 -0500

    done adding original implementation

--- data/wikitext_data.py
+++ data/wikitext_data.py
@@ -3,11 +3,11 @@ def get_wikitext_data():
     wikitext_dataset = load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1")
     return wikitext_dataset
 
-# Function to save data so that tokenization can be saved
+# Save data so that tokenization can be saved
 def save_data(dataset, path):
     dataset.save_to_disk(path)
 
-# Function to reload the data from local storage
+# Reload the data from local storage
 def load_data(path):
     from datasets import load_from_disk
     return load_from_disk(path)
